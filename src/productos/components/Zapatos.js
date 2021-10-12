37a307fd7162f0054f98cb9c0b45a72d095644ad
MCamilaBS <mcamila.bernals@outlook.com>
2021-10-11 22:00:14 -0500

    Cambio productos

--- src/productos/components/Zapatos.js
+++ src/productos/components/Zapatos.js
@@ -1,22 +1,27 @@
-import Card from 'react-bootstrap/Card'
-import Button from 'react-bootstrap/Button'
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+
+
+
 
 
 
 const Zapatos1 = ({Zapatos}) => {
     return(
             <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src={Zapatos.url} />
+        <Card.Header><center><h2>{Zapatos.marca}</h2></center></Card.Header>
+        <Card.Img variant="top" src={Zapatos.url} width={300} height={300} alt="300x300" rounded /> 
         <Card.Body>
             <Card.Title>{Zapatos.title}</Card.Title>
             <Card.Text>
               {Zapatos.description}
             </Card.Text>
-            <Button variant="danger">${Zapatos.price}</Button>
+            <Button variant="danger" size="sm">${Zapatos.price}</Button> 
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <Button variant="danger" size="sm">{Zapatos.category}</Button>
         </Card.Body>
     </Card>
     )
-
 };
 
 
---
+++ src/productos/components/img/index.js
@@ -0,0 +1,2 @@
+export const BBYH1 = require("../img/BBY_H_1.jpg")
+export const BBYH2 = require("../img/BBY_H_2.jpg")
