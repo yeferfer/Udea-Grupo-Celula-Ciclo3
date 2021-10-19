99f51d83ee39eb2f00b2d343b9ee8e8d89150e57
Yeferson Ferney Culma Montoya <jefer.1142@gmail.com>
2021-10-18 22:07:23 -0500

    Update

--- Frontend/src/ventas/pages/VentasRealizadas.js
+++ Frontend/src/ventas/pages/VentasRealizadas.js
@@ -1,178 +1,93 @@
-import React from "react";
-import Button from "react-bootstrap/Button";
+import { Container, Table, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import api from "../../api";
+import "./Gestion.css";
 
+const VentasRealizadas = ({ ventas, setVentas }) => {
+  const deleteVentas = (event) => {
+    const id = event.target.id;
+    api.ventas.delete(id);
+    console.log(ventas);
+    const newVentas = ventas.filter((venta) => venta._id !== id);
+    setVentas([...newVentas]);
+  };
 
+  return (
+    <div>
+      <h1 className="text-center mt-5 mb-5">Gestión de Ventas</h1>
+      <Container>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>Fecha Ventas</th>
+              <th>Producto</th>
+              <th>Referncia</th>
+              <th>Precio</th>
+              <th>Descripcion</th>
+              <th>Sucursal</th>
+              <th>Vendedor</th>
+              <th>Categoria</th>
+              <th>Acción</th>
+            </tr>
+          </thead>
+          <tbody>
+            {ventas.map((venta) => {
+              return (
+                <tr key={venta._id}>
+                  <td>{venta.Fecha_Venta}</td>
+                  <td>{venta.Producto}</td>
+                  <td>{venta.Referencia}</td>
+                  <td>{venta.Precio}</td>
+                  <td>{venta.Descripcion}</td>
+                  <td>{venta.Sucursal}</td>
+                  <td>{venta.Vendedor}</td>
+                  <td>{venta.Categoria}</td>
+                  <td>
+                    <Link to={`/Gestion/Edit/${venta._id}`}>
+                      <Button variant="warning">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          className="bi bi-pencil"
+                          viewBox="0 0 16 16"
+                        >
+                          <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168l10-10zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207 11.207 2.5zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293l6.5-6.5zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325z" />
+                        </svg>
+                      </Button>
+                    </Link>
 
-const VentasRealizadas = () => {
-
-    return <div className="container mt-3" id="mi_tabla" >
-        <h1 className="text-center mt-2 mb-2"> Ventas Realizadas</h1>
-        <table className="table table-striped table-hover" border="4" >
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Fecha Venta</th>
-                    <th scope="col">Producto</th>
-                    <th scope="col">Referencia</th>
-                    <th scope="col">Precio</th>
-                    <th scope="col">Descripcion</th>
-                    <th scope="col">Sucursal</th>
-                    <th scope="col">Vendedor</th>
-                    <th scope="col">Categoria</th>
+                    <Button
+                      variant="danger"
+                      onClick={deleteVentas}
+                      id={venta._id}
+                      className="ms-2"
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        className="bi bi-trash"
+                        viewBox="0 0 16 16"
+                      >
+                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z" />
+                        <path
+                          fillRule="evenodd"
+                          d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"
+                        />
+                      </svg>
+                    </Button>
+                  </td>
                 </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <th scope="row">3</th>
-                    <td colspan="1">Larry the Bird</td>
-                    <td>@twitter</td>
-                    <td>@twitter</td>
-                    <td>@twitter</td>
-                    <td>@twitter</td>
-                    <td>@twitter</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <th scope="row">4</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <th scope="row">5</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <th scope="row">6</th>
-                    <td colspan="1">Larry the Bird</td>
-                    <td>@twitter</td>
-                    <td>@twitter</td>
-                    <td>@twitter</td>
-                    <td>@twitter</td>
-                    <td>@twitter</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <th scope="row">7</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <th scope="row">8</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <th scope="row">9</th>
-                    <td colspan="1">Larry the Bird</td>
-                    <td>@twitter</td>
-                    <td>@twitter</td>
-                    <td>@twitter</td>
-                    <td>@twitter</td>
-                    <td>@twitter</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <th scope="row">10</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <th scope="row">11</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                </tr>
-                <tr>
-                    <th scope="row">12</th>
-                    <td colspan="1">Larry the Bird</td>
-                    <td>@twitter</td>
-                    <td>@twitter</td>
-                    <td>@twitter</td>
-                    <td>@twitter</td>
-                    <td>@twitter</td>
-                    <td>@mdo</td>
-                    <td>@mdo</td>
-                </tr>
-            </tbody>
-        </table>
-        <Button type="button" variant="outline-secondary">
-            Modificar
-        </Button>
-        {/* <Button type="button" variant="danger" className="float-end"> Buscar </Button> */}
-        <input type="text" class="form-control " variant="danger" className="float-end" Buscar />
-        <Button type="button" variant="danger" className="float-end"> Buscar </Button>
-       
-
-
-        <Button
-            type="button"
-            variant="danger"
-            className="float-lg-start"
-        >
-            Eliminar
-        </Button>
+              );
+            })}
+          </tbody>
+        </Table>
+      </Container>
     </div>
-
+  );
 };
+
 export default VentasRealizadas;
